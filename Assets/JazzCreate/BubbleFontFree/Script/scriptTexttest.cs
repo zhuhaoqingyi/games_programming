e3aa8a66726c2f422482248f8bf49da4eacfc788
chenrui <2617363@dundee.ac.uk>
2026-04-27 23:57:24 +0800

    MainMenu

---
+++ Assets/JazzCreate/BubbleFontFree/Script/scriptTexttest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+namespace JazzCreateGames.BubbleFont.Free
+{
+    public class ScriptTextTest : MonoBehaviour
+    {
+        public TMP_Text bubbleText;
+        public TMP_Text bubbleTitleTxt;
+        public GameObject bubble;
+        public GameObject bubbleTitle;
+
+        void Start()
+        {
+            bubbleTitleTxt.text = "Font JazzCreate Bubble";
+
+            bubbleText.text =
+                "☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼▬↨↑↓→←\n" +
+                "∟↔▲▼!#$%&'()*+,-./0123456789:;<=>?@\n" +
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n" +
+                "`abcdefghijklmnopqrstuvwxyz\n" +
+                "îìÄÅÉæÆôöòûùÿÖÜ \n" +
+                "¢£¥ƒáíóúñÑªº¿¬½¼¡«»░▒▓\n" +
+                "│┤╣║╗┐└┴┬├─┼╚╔╩╦╣║╗•§©®¶_\n" +
+                "Thanks for using fonts from JazzCreatesGames 2015–2025 ©.";
+        }
+    }
+}
